442741ba7b965007c8fd6c0ab774efbe7f8d0bbd
LuHai <lhai0704@gmail.com>
2024-10-16 11:25:59 +0800

    rebuild project

--- src/main/java/com/energytrading/decentralized_energy_trading/repository/EnergyDataRepository.java
+++
@@ -1,7 +0,0 @@
-package com.energytrading.decentralized_energy_trading.repository;
-
-import com.energytrading.decentralized_energy_trading.model.EnergyData;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EnergyDataRepository extends CrudRepository<EnergyData, Long> {
-}
